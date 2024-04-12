0063e9c2cd78ea91725bf212415deaa5c7f4eaec
fits-soeno-fm <61449986+Fumiya-Soeno@users.noreply.github.com>
2024-04-12 16:00:55 +0900

    カード装飾情報の取得API

---
+++ pages/api/card_decorations/show.js
@@ -0,0 +1,34 @@
+import { sql } from "@vercel/postgres";
+import { authenticate } from "../../helpers/authenticate";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  const authResult = await authenticate(req);
+
+  if (!authResult.valid) {
+    return res
+      .status(401)
+      .json({ success: false, message: "トークンの検証に失敗しました" });
+  }
+
+  const { card_id } = req.body;
+  if (!card_id) {
+    return res.status(400).json({
+      success: false,
+      message: "card_idが指定されていません",
+    });
+  }
+
+  // カード装飾レコードの取得(SELECT)処理
+  const records =
+    await sql`SELECT * FROM card_decorations WHERE card_id = ${req.body.card_id}`;
+
+  return res.status(200).json({
+    success: true,
+    accessToken: authResult.accessToken,
+    records: records.rows,
+  });
+}
