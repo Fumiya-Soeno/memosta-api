8c3c5712da3f3d2bca5979052c2f5580729d1193
fits-soeno-fm <61449986+Fumiya-Soeno@users.noreply.github.com>
2024-04-21 13:46:58 +0900

    不要なファイルの削除

--- pages/api/users.js
+++
@@ -1,9 +0,0 @@
-// pages/api/users.js
-export const users = [
-  {
-    id: 1,
-    email: "user@example.com",
-    password: "password", // 注意: 実際のアプリケーションではパスワードはハッシュ化して保存します
-  },
-  // 他のユーザーデータ
-];
