9fc02a1382cf99aa41fd6d9f2860ad3399576e03
fits-soeno-fm <soeno.fm@fits-inc.jp>
2025-01-23 08:47:49 +0900

    SidebarLinkのリンク先が内部リンクの場合にLinkタグを使うように修正

--- src/app/components/common/SidebarLink.tsx
+++ src/app/components/common/SidebarLink.tsx
@@ -1,16 +1,35 @@
 "use client";
 
+import Link from "next/link";
+
 interface SidebarLinkProps {
   href: string;
   text: string;
 }
 
 export function SidebarLink({ href, text }: SidebarLinkProps) {
+  // 外部リンクかどうかを自動判別
+  const isExternal = href.startsWith("http://") || href.startsWith("https://");
+
   return (
     <li className="my-4">
-      <a href={href} className="text-gray-800 no-underline">
-        {text}
-      </a>
+      {isExternal ? (
+        <a
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-800 no-underline hover:underline"
+        >
+          {text}
+        </a>
+      ) : (
+        <Link
+          href={href}
+          className="text-gray-800 no-underline hover:underline"
+        >
+          {text}
+        </Link>
+      )}
     </li>
   );
 }
