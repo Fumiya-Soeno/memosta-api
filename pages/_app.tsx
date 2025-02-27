d537a36f308e3027c934e60907dd1401d71a66bf
fits-soeno-fm <soeno.fm@fits-inc.jp>
2025-02-27 18:48:13 +0900

    ビルドエラーの修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
+import type { AppProps } from "next/app";
+
 // TailwindCSSをpagesディレクトリ以下に読み込ませるためのファイル
 import "../src/app/globals.css";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
