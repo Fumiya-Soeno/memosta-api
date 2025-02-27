d93059ef480a1f692531555977f6d776e3c9ba75
fits-soeno-fm <soeno.fm@fits-inc.jp>
2025-02-27 20:10:40 +0900

    ビルドエラー修正

--- pages/api/cards/show.js
+++ pages/api/cards/show.js
@@ -1,5 +1,5 @@
 import { sql } from "@vercel/postgres";
-import { authenticate } from "../../helpers/authenticate";
+import { authenticate } from "../../../helpers/authenticate";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
